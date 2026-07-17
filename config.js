095c19dcbed6a878b309675349f8b9f99a72bfad
www-icloud-net <systemprojects7@gmail.com>
2026-07-17 19:13:04 +0000

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.NIS_CONFIG = Object.freeze({
-  supabaseUrl: "https://krvptqnwkmxlmwnxrdgp.supabase.co",
-  supabaseAnonKey: "sb_publishable_bvBPicI-E8xwl-TVqnlbkQ_0N4Dc1cC",
-  appName: "Nipe International School Report Card System",
-  logoPath: "assets/nipe-school-logo.png"
-});
