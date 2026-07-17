fd241ed97a50ecece5f77db3ce92bfee23c517a8
www-icloud-net <systemprojects7@gmail.com>
2026-07-17 14:58:24 +0000

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.NIS_CONFIG = Object.freeze({
-  supabaseUrl: "YOUR_SUPABASE_URL",
-  supabaseAnonKey: "YOUR_SUPABASE_ANON_KEY",
+  supabaseUrl: "https://krvptqnwkmxlmwnxrdgp.supabase.co",
+  supabaseAnonKey: "sb_publishable_bvBPicI-E8xwl-TVqnlbkQ_0N4Dc1cC",
   appName: "Nipe International School Report Card System",
   logoPath: "assets/nipe-school-logo.png"
 });
